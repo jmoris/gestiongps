14fa4bf91d87353a43c917591b7ee50728efbdf2
Jesus Moris <jesus@soluciontotal.cl>
2019-05-05 22:06:41 -0400

    Se agrega boton toggle para colapsar y boton salir + se deja fixed el nav y se baja un poco el contenedor.

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -11,11 +11,16 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
   </head>
   <body>
-    <nav class="navbar navbar-expand-sm bg-azul navbar-dark">
+    <nav class="navbar navbar-expand-sm bg-azul fixed-top navbar-dark">
       <a class="navbar-brand" href="#">
         <img src="/logo.png" alt="logo" style="width:200px;">
       </a>
-      <ul class="navbar-nav">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse"
+              aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarCollapse">
+      <ul class="navbar-nav mr-auto">
         <li class="nav-item">
           <a class="nav-link" href="#"><img width="24px" src="/icon/herramientas.png"/>Ajustes</a>
         </li>
@@ -38,8 +43,20 @@
           <a class="nav-link" href="#"><img width="24px" src="/icon/servidor.png"/>Servidor</a>
         </li>
       </ul>
+      <ul class="navbar-nav px-3">
+          <li class="nav-item text-nowrap">
+              <a class="nav-link" href="/logout" onclick="event.preventDefault();
+              document.getElementById('logout-form').submit();">
+              <i class="fas fa-sign-out-alt"></i>
+              Salir</a>
+          </li>
+          <form id="logout-form" action="/logout" method="POST" style="display: none;">
+              @csrf
+          </form>
+      </ul>
+          </div>
     </nav>
-
+    <div class="espacio"></div>
     <main role="main" class="container">@yield("contenido")</main>
   </body>
 </html>
