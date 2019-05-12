c25e7c735c433895123b99645d1babce838305fb
Rodrigo <rodrigocorderodiaz@gmail.com>
2019-05-12 01:25:24 -0400

    Se agrega la obtencion de la configuracion del servidor

--- app/Http/Controllers/ServidorController.php
+++ app/Http/Controllers/ServidorController.php
@@ -7,21 +7,29 @@ use Illuminate\Http\Request;
 class ServidorController extends Controller
 {
 
-    public function obtener(Requiest $request)
+    public function obtener()
     {
-        
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, env('API_ENDPOINT').'/server');
+        curl_setopt($ch, CURLOPT_POST, FALSE);
+        curl_setopt($ch, CURLOPT_USERPWD, \Session::get(‘email’). ":" . \Session::get(‘password));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $respuesta = curl_exec ($ch);
+        curl_close ($ch);
+        return view('servidor', ['servidor' => json_decode($respuesta)]);
     }
 
-    public function guardar(){
+    public function guardar(Request $request){
         
         $fields = [
-        '$check_servidor' => $request->check_servidor,
-        '$check_dispositivo' => $request->check_dispositivo,
-        '$latitud' => $request->latitud,
-        '$longitud' => $request->longitud,
+        '$registration' => $request->check_servidor,
+        '$deviceReadonly' => $request->check_dispositivo,
+        '$latitude' => $request->latitud,
+        '$longitude' => $request->longitud,
         '$zoom' => $request->zoom
         ];
 
+        $fields_string = http_build_query($fields);
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, env('API_ENDPOINT').'/server');
         curl_setopt($ch, CURLOPT_POST, FALSE);
--- resources/views/servidor.blade.php
+++ resources/views/servidor.blade.php
@@ -8,7 +8,7 @@
     <h5 class="card-header">Ajustes del servidor:</h5>
     <div class="espacio-20"></div>
     <div class="col-md-8 offset-md-2">
-    <form action="acciones_servidor.blade.php" method="post">
+    <form action="/servidor" method="put">
         <div class="form-group row">
             <div class="col-sm-4">Servidor en modo lectura</div>
             <div class="col-sm-8">
