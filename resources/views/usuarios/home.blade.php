fa0ca131a327fcf3dadebd67c5828e5052b2e3a6
COMPARE MONCHO <matias.escobar.c@gmail.com>
2019-05-12 20:59:36 -0400

    crear interfaz para el crud del usuario

--- resources/views/usuarios/home.blade.php
+++ resources/views/usuarios/home.blade.php
@@ -3,8 +3,9 @@
 @section('titulo', 'Lista de usuarios')
 
 @section('contenido')
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#addUsr" style="margin: 0px 10px 10px 0px">Agregar usuario</button>
     <div class="card">
-        <h5 class="card-header">Lista de Usuarios</h5>
+        <h5 class="card-header">Usuarios</h5>
         <div class="card-body">
             <div class="table-responsive">
                 <table id="tablaDatos" class="table table-sm table-hover">
@@ -25,9 +26,9 @@
                     <td>{{ $usuario->phone}}</td>
                     <td>{{ ($usuario->administrator) ? 'SI':'NO' }}</td>
                     <td>
-                        <a class="btn btn-sm btn-outline-primary" title="Ver información" href="#">
-                            ver
-                        </a>
+                        <a class="btn btn-sm btn-outline-primary" title="Ver información" href="#">ver</a>
+                        <a class="btn btn-sm btn-outline-danger" title="Ver información" href="#">borrar</a>
+                        <a class="btn btn-sm btn-outline-secondary" title="Ver información" href="#">modificar</a>
                     </td>
                 </tr>
                 @endforeach
@@ -36,4 +37,59 @@
             </div>
         </div>
     </div>
+    {{-- Formulario para agregar user --}}
+    <div class="modal fade" id="addUsr" tabindex="-1" role="dialog" aria-labelledby="addUsr" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalLongTitle">Crear Usuario</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div class="modal-body">
+                        <form>
+                                <div class="form-group">
+                                        <label for="nombre">Nombre</label>
+                                        <input type="text" class="form-control" id="nombre" placeholder="Nombre usuario">
+                                </div>
+                                <div class="form-row">
+                                    <div class="form-group col-md-6">
+                                        <label for="inputEmail">Email</label>
+                                        <input type="email" autocomplete="off" class="form-control" id="inputEmail" placeholder="Email">
+                                    </div>
+                                    <div class="form-group col-md-6">
+                                        <label for="inputPassword">Constraseña</label>
+                                        <input type="password" autocomplete="off" class="form-control" id="inputPassword" placeholder="Contraseña">
+                                    </div>
+                                </div>
+                                <div class="form-row">
+                                        <div class="form-group col-md-5">
+                                                <p>Fecha de expiración: <input type="text" id = "datepicker"></p>
+                                          </div>
+                                </div>
+                                <div class="form-group">
+                                  <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" id="permLect">
+                                    <label class="form-check-label" for="permLect">
+                                      Permisos de lectura
+                                    </label>
+                                  </div>
+                                  <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" id="editDisp">
+                                    <label class="form-check-label" for="editdisp">
+                                        Permisos para editar datos de dispositivos
+                                    </label>
+                                  </div>
+                                </div>
+                              </form>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+                  <button type="button" class="btn btn-primary">Crear</button>
+                </div>
+              </div>
+            </div>
+          </div>
+
 @stop
