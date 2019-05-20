df15805fa40bd539b9aa9665e9fdbcdcd629594d
COMPARE MONCHO <matias.escobar.c@gmail.com>
2019-05-19 21:49:34 -0400

    cambios pequeños

--- resources/views/usuarios/home.blade.php
+++ resources/views/usuarios/home.blade.php
@@ -3,7 +3,7 @@
 @section('titulo', 'Lista de usuarios')
 
 @section('contenido')
-<a type="button" href="/usuarios/agregar-usuario" class="btn btn-primary" style="margin: 0px 10px 10px 0px">Agregar usuario</a>
+<a href="/usuarios/agregar-usuario" class="btn btn-primary" style="margin: 0px 10px 10px 0px">Agregar usuario</a>
     <div class="card">
         <h5 class="card-header">Usuarios</h5>
         <div class="card-body">
